a2530d94262c746844d609faf4ff94e3c8ffc701
will-friend <wsfriend3@gmail.com>
2025-11-05 23:20:06 -0500

    Docstring added for EventStudy class and methods

--- src/analysis/EventStudy.py
+++ src/analysis/EventStudy.py
@@ -5,12 +5,46 @@ from src.analysis.utils import rolling_residual
 
 
 class EventStudy:
+    """
+    Class that implements event driven returns study using CARs and t-tests.
+
+    Attributes
+    ----------
+    ticker : str
+        String representing the ticker of the asset of the study
+    market_returns : pd.Series
+        Time series of our market index's returns
+    car_windows : list[int]
+        List of windows to calculate CAR over for the tests
+    residuals : pd.Series
+        Time series of the residuals of regressing the asset being tested's
+        returns on `market_return`, representing abnormal returns
+
+    """
+
     def __init__(
         self,
         ticker: str,
         market_returns: pd.Series,
-        car_windows: list = [1, 5, 10, 20, 30],
+        car_windows: list[int] = [1, 5, 10, 20, 30],
     ) -> None:
+        """
+        Constructor method to build EventStudy object with proper
+        instance attributes
+
+        Parameters
+        ----------
+        ticker : str
+            String representing the ticker of the asset of the study
+        market_returns : pd.Series
+            Time series of our market index's returns
+        car_windows : list[int]
+            List of windows to calculate CAR over for the tests
+
+        Returns
+        -------
+        None
+        """
         self.ticker = ticker
         self.market_returns = market_returns
         self.car_windows = car_windows
@@ -22,14 +56,53 @@ class EventStudy:
         window: int = 35,
         expanding: bool = True,
     ) -> pd.Series:
+        """
+        Method to perform rolling regression of asset returns on
+        `self.market_returns`, and set the `self.residuals` attribute
+        with the resulting regression residuals
+
+        Parameters
+        ----------
+        stock_returns : pd.Series
+            Time series of the returns of the asset of the study (`self.ticker`)
+        window : int
+            Window to run the rolling regression over
+        expanding : bool
+            Boolean flag indicating if you want expanding to be turned on for the
+            rolling regressing
+
+        Returns
+        -------
+        None
+        """
         self.residuals = rolling_residual(
             stock_returns, self.market_returns, window, expanding
         )
         return self
 
     def event_single_test(
-        self, event_dates: list, pop_mean: float = 0.0
+        self, event_dates: list[pd.Timestamp], pop_mean: float = 0.0
     ) -> pd.DataFrame:
+        """
+        Method to run a single-sample t-test, using the calculated
+        abnormal returns (`self.residuals`) and a provided population
+        mean
+
+        Parameters
+        ----------
+        event_dates : list[pd.Timestamp]
+            List of event dates to calculate CARs for
+        pop_mean : float
+            Float representing the population mean as the comparison value
+            in the t-test
+
+        Returns
+        -------
+        results : pd.DataFrame
+            DataFrame containing resulting statistics and scores from the
+            single-sample t-test
+        """
+
         results = single_sample_test(
             self.residuals, event_dates, self.car_windows, pop_mean
         )
@@ -43,6 +116,36 @@ class EventStudy:
         pop_mean: float = 0.0,
         replacement: bool = False,
     ) -> pd.DataFrame:
+        """
+        Method to run a single-sample t-test on pseudo event dates,
+        using the calculated abnormal returns (`self.residuals`) and
+        a provided population mean, and return aggregated test results
+        over the trials run
+
+        Parameters
+        ----------
+        filtered_dates : list[pd.Timestamp]
+            List of pseudo dates to sample randomly from
+        n : int
+            Number of samples to take per trial
+        M : int
+            Number of trials to run
+        pop_mean : float
+            Float representing the population mean as the comparison value
+            in the t-test
+        replacement : bool
+            Boolean flag indicating if random sampling shold be done with
+            replacement
+
+        Returns
+        -------
+        mean_random : float
+            Aggregate mean of t-score from `M` single-sample t-tests of randomly
+            sampled pseudo-events
+        std_random : float
+            Aggregate standard deviation of t-score from `M` single-sample t-tests
+            of randomly sampled pseudo-events
+        """
         random_results = pd.DataFrame(
             index=self.car_windows, columns=["t{}".format(i) for i in range(M)]
         )
@@ -75,6 +178,36 @@ class EventStudy:
         M: int = 1000,
         replacement: bool = False,
     ) -> pd.DataFrame:
+        """
+        Method to run a two-sample t-test, comparing CAR of event dates
+        to CAR of pseudo event dates using the calculated abnormal returns
+        (`self.residuals`), and return
+        aggregated test results over the trials run
+
+        Parameters
+        ----------
+        event_dates : list[pd.Timestamp]
+            List of event dates to calculate CARs for
+        filtered_dates : list[pd.Timestamp]
+            List of pseudo dates to sample randomly from
+        n : int
+            Number of samples to take per trial
+        M : int
+            Number of trials to run
+        replacement : bool
+            Boolean flag indicating if random sampling shold be done with
+            replacement
+
+        Returns
+        -------
+        mean_random : float
+            Aggregate mean of t-score from `M` single-sample t-tests of randomly
+            sampled pseudo-events
+        std_random : float
+            Aggregate standard deviation of t-score from `M` single-sample t-tests
+            of randomly sampled pseudo-events
+        """
+
         two_sample_results = pd.DataFrame(
             index=self.car_windows, columns=["t{}".format(i) for i in range(M)]
         )
